b46d93308af995af2fab170496276705550757e3
Tommy <artem102040@gmail.com>
2020-01-21 17:53:06 +0300

    Add cool face API

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ express()
   .get('/dbtest', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM users');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
