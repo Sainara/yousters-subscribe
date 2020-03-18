f26f9fa7d439236f21ea6c8faf979af277902d75
Tommy <artem102040@gmail.com>
2020-03-18 22:16:48 +0600

    Add cool face API

--- index.js
+++ index.js
@@ -21,15 +21,7 @@ var fs = require('fs');
 
 var multer = require('multer')
 
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, '/uploads/')
-  },
-  filename: function(req, file, cb) {
-    console.log(file)
-    cb(null, file.originalname)
-  }
-})
+var storage = multer.diskStorage()
 
 
 let lawyers = {
