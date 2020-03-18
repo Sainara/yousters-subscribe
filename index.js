fe3a1ba0e23f52d765be041cc44a1f435e892239
Tommy <artem102040@gmail.com>
2020-03-18 23:40:26 +0600

    Add cool face API

--- index.js
+++ index.js
@@ -2456,6 +2456,7 @@ express()
    .post('/adduser', parser.single('image'), async (req, res) => {
       try {
         const client = await pool.connect()
+        console.log(req.body.is_ava_sel)
         if (req.body.is_ava_sel) {
           const result = await client.query('INSERT INTO users (login, name, password, user_type, email, city, phone, ava) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
          [req.body.login, req.body.name, req.body.pass, req.body.type, req.body.email, req.body.city, req.body.phone, req.file.url]);
