4dad4529a28fd9a110072413236f0eac99bc7893
Tommy <artem102040@gmail.com>
2020-03-18 20:46:15 +0600

    Add cool face API

--- index.js
+++ index.js
@@ -2389,7 +2389,7 @@ express()
       //   res.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
       //            cloudinary.image(result.public_id, { format: "png", width: 100, height: 130, crop: "fill" }));
     //   }, { public_id: req.body.title }
-     );
+  });
       ///////
     fs.createReadStream(req.files.image.path, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
   });
