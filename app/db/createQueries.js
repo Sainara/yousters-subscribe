27d3db611c39219d3ab7a1af802b5ea5ae470356
Tommy <artem102040@gmail.com>
2020-07-21 02:00:38 +0600

    update

--- app/db/createQueries.js
+++ app/db/createQueries.js
@@ -1,17 +1,29 @@
 import dbQuery from '../db/dbQuery';
 
-const create_entersessions = async () => {
-  var query = 'CREATE TABLE entersessions(id serial PRIMARY KEY, sessionID VARCHAR, code VARCHAR, tryCounter int, expiretime timestamptz, number VARCHAR)';
+const createTable(tableName, query) {
+
+  const checkExistQuery = 'SELECT to_regclass($1);';
 
   try {
-    const {rows} = await dbQuery.query(query, []);
-  } catch (e) {
+    const check = await dbQuery.query(checkExistQuery, [tableName]);
+    const dbResponse = check[0];
 
+    if (!dbResponse) {
+      const create = await dbQuery.query(query, [tableName]);
+      console.log(tableName + " created");
+    } else {
+      console.log(tableName + " exist");
+    }
+  } catch (error) {
+    console.error(error);
   }
-}
+};
 
 export default {
   initDBs() {
-    console.log('getPaymentQuery');
+    var tableName = 'entersessions';
+    var query = 'CREATE TABLE $1(id serial PRIMARY KEY, sessionID VARCHAR, code VARCHAR, tryCounter int, expiretime timestamptz, number VARCHAR)';
+    createTable(tableName, query);
+    console.log('creating');
   }
 };
