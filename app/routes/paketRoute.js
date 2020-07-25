803bbd8d62619735b919baf6a1ca85985a0c0dc4
Tommy <artem102040@gmail.com>
2020-07-26 04:36:58 +0600

    pakets

--- app/routes/paketRoute.js
+++ app/routes/paketRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
-import { selectPaketsQuery } from '../controllers/paketController';
+import { getAviablePakets } from '../controllers/paketController';
 import verifyAuth from '../middlewares/verifyAuth';
 
 const router = express.Router();
 
 // router.post('/payment', verifyAuth, createPayment);
-router.get('/pakets', verifyAuth, selectPaketsQuery);
+router.get('/pakets', verifyAuth, getAviablePakets);
 //console.log(uploader);
 //router.post('/validate', validate);
 
