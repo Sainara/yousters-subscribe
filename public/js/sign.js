8f57b15b403a132f78b5a19b72288d2cfbbe6313
Tommy <artem102040@gmail.com>
2020-07-30 03:30:12 +0600

    added front

--- public/js/sign.js
+++ public/js/sign.js
@@ -9,19 +9,21 @@ $(document).ready(function () {
 	      method: "POST", // метод HTTP, используемый для запроса
 	      url: "https://you-scribe.ru/api/v1/auth", // строка, содержащая URL адрес, на который отправляется запрос
         contentType: "application/json",
-	      data: { // данные, которые будут отправлены на сервер
-	        phone: "+79139726630"
-	      },
-	      success: function ( msg ) { // функции обратного вызова, которые вызываются если AJAX запрос выполнится успешно (если несколько функций, то необходимо помещать их в массив)
-	        alert(msg) // добавляем текстовую информацию и данные возвращенные с сервера
-	      },
+        dataType: 'json',
+	      data: JSON.stringify({ // данные, которые будут отправлены на сервер
+	        number: "+79139726630"
+	      }),
 	      statusCode: {
-	        200: function () { // выполнить функцию если код ответа HTTP 200
-	          console.log( "Ok" );
+	        200: function (res) { // выполнить функцию если код ответа HTTP 200
+	          if (res.success) {
+              sessionID = res.sessionid;
+
+            }
 	        }
 	      }
 	    })
     });
+
 
 
 });
