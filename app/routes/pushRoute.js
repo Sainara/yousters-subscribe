4aab7751ff353b5ed82e9b873ff43eb8aafd1593
Tommy <artem102040@gmail.com>
2020-08-01 15:28:42 +0600

    sendPush

--- app/routes/pushRoute.js
+++ app/routes/pushRoute.js
@@ -14,7 +14,7 @@ router.post('/token', primaryLimit, verifyAuth, addDeviceToken);
 router.delete('/token', primaryLimit, verifyAuth, removeDeviceToken);
 
 router.post('/token/test', async (req, res) => {
-  sendNotification('title', 'body', 1, {deepLink:"https://you-scribe.ru/case/79c2419b-06e3-4990-b1c0-8e77c5be9934"});
+  sendNotification('title', 'Activation', 1, {deepLink:"https://you-scribe.ru/profileactivation"});
   res.send({result : true});
 });
 
