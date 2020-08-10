60bb0088a2b3f34c681d9c15aeacdd9819285cb0
Tommy <artem102040@gmail.com>
2020-08-10 22:45:53 +0600

    update legal

--- publicApp/routes/legalRoute.js
+++ publicApp/routes/legalRoute.js
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/user-agreement', (req, res) =>
-  res.render('pages/legal/user-agreement/user-agreement', {page_title: "Лицензионное соглашение"})
+  res.render('pages/legal/user-agreement/user-agreement', {page_title: "Лицензионное соглашение для мобильных устройств"})
+);
+router.get('/user-agreement/web', (req, res) =>
+  res.render('pages/legal/user-agreement/user-agreement-web', {page_title: "Лицензионное соглашение для веб-приложения"})
 );
 router.get('/termsofuse', (req, res) =>
   res.render('pages/legal/termsofuse/termsofuse', {page_title: "Условия использования"})
