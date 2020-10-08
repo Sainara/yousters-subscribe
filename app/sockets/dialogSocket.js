f5a6a25ee7b5be61f459d51f824c368960815b39
Tommy <artem102040@gmail.com>
2020-10-08 21:26:22 +0300

    add dialog base logic

--- app/sockets/dialogSocket.js
+++ app/sockets/dialogSocket.js
@@ -69,6 +69,31 @@ const connectToDialog = async (ws, req) => {
     ws.on('message', function(msg) {
       //console.log(msg);
 
+
+      var Busboy = require('busboy');
+
+      var busboy = new Busboy();
+        busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+          console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+          file.on('data', function(data) {
+            console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+          });
+          file.on('end', function() {
+            console.log('File [' + fieldname + '] Finished');
+          });
+        });
+        busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+          console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+        });
+        busboy.on('finish', function() {
+          console.log('Done parsing form!');
+          //res.writeHead(303, { Connection: 'close', Location: '/' });
+          //res.end();
+        });
+        msg.pipe(busboy);
+
+      return
+
       const boundary = getBoundary(msg.toString('utf8'))
         if (!boundary) {
           endRequestWithError(response, body, 400, 'Boundary information missing', callback)
