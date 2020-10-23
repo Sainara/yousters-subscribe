a33ea89a9349e72d59b9b26886d10deae1a94c99
Tommy <artem102040@gmail.com>
2020-10-24 04:49:09 +0600

    lawyerPage found

--- app/helpers/checkers.js
+++ app/helpers/checkers.js
@@ -22,7 +22,7 @@ const isLawyer = async (req, res, next) => {
   const errorMessage = Object.assign({}, eMessage);
 
   const lawyerLVLs = ['economy, profi, premium'];
-  console.log(req.user);
+  console.log(lawyerLVLs.includes(req.user.level));
   if (lawyerLVLs.includes(req.user.level)) {
     next();
   } else {
